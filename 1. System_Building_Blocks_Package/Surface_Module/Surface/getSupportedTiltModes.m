b7439479d215f3a6b44f70cb782ba23defc41cef
Norman G. Worku <normangirma2012@gmail.com>
2015-08-11 17:47:45 +0200

    add None coating definition which is the same as NullCoating

---
+++ 1. System_Building_Blocks_Package/Surface_Module/Surface/getSupportedTiltModes.m
@@ -0,0 +1,4 @@
+function supportedTiltModes = getSupportedTiltModes()
+    supportedTiltModes = {'DAR' 'NAX' 'BEN'};
+end
+
---
+++ PROGRESS_Files/PROGRESS_GaussianDecomposition1D/TestGaussianDecompOfRectangularSignal.m
@@ -0,0 +1,92 @@
+function [optimalValue,optimalMeritFunction,optimizedObject,outputStruct] = ...
+        Case2_GeneralGaussians(signalAmplitudeVector,signalCenter,signalWidth,signalEdgeFactor,nGauss )
+    if nargin == 0
+        % Make a rectangular signal
+        myMaxAmplitude = [1];
+        myCenter = [0];
+        myWidth = [10];
+        myEdgeFactor = 0.2;
+        
+        % Fixed parameters
+        nGauss = 20; % shall be odd
+        
+        nSampling = 500;
+        nx = nSampling;
+        lowerXTotal = myCenter - (myEdgeFactor + 0.5)*myWidth;
+        upperXTotal = myCenter + (myEdgeFactor + 0.5)*myWidth;
+        xlinTotal = linspace(lowerXTotal,upperXTotal,nx);
+        
+       
+        
+        signalCenter = myCenter;
+        signalWidth = myWidth;
+        signalEdgeFactor = myEdgeFactor;
+        
+%         % Rectangle
+         myRect1D =  Rect1D(myMaxAmplitude,myCenter,myWidth,myEdgeFactor);
+         signalAmplitudeVector = computeRectAmplitude1D( myRect1D,xlinTotal );
+%        % Parabola
+%         signalAmplitudeVector = -myMaxAmplitude*(xlinTotal-signalCenter).^2;
+        % Plot input
+        figure; plot(xlinTotal,signalAmplitudeVector);
+    end
+    
+    optimizationAlgorithmName = 'steepestDescentOptimizer';
+    meritFunctionName = 'GaussianDecomposition1DRMSE';
+    targetValue = 0;
+    weight = 1;
+    
+    gaussAmplitude = [1,0,-Inf,Inf];
+    gaussCenter = [0,0,-Inf,Inf];
+    gaussWaistRadius = [1,0,-Inf,Inf];
+    
+    gauss1DArray = Gauss1D( gaussAmplitude,gaussCenter,gaussWaistRadius,nGauss );
+    
+    % Make the center, width and amplitude optimizable variables
+    gauss1DArray.Amplitude(:,2) = ones(nGauss,1);
+    gauss1DArray.Center(:,2) = ones(nGauss,1);
+    gauss1DArray.WaistRadius(:,2) = ones(nGauss,1);
+    optimizableObject = gauss1DArray;
+    
+    tolx = 10^-3;
+    tolfun = 10^-3;
+    outputFuncHandle = @(x)x; % just nothing
+    maxIter = 1000;
+    optimizationOption = struct();
+    optimizationOption.TolX = tolx;
+    optimizationOption.TolFun = tolfun;
+    optimizationOption.OutputFcn = outputFuncHandle;
+    optimizationOption.MaximumIteration = maxIter;
+    
+    meritFunctionParameters = struct();
+    meritFunctionParameters.nGauss = nGauss;
+
+    meritFunctionParameters.SignalCenter = signalCenter;
+    meritFunctionParameters.SignalAmplitudeVector = signalAmplitudeVector;
+    meritFunctionParameters.SignalWidth = signalWidth;
+    meritFunctionParameters.SignalEdgeFactor = signalEdgeFactor;
+    
+    % Get the initial center and width using a constant width to spacing ratio
+    waistRadiusToSpacing = 1.5;
+    % Assume the rectangle edge is on the waist of the last gausslets
+    initialGaussWaistRadius = (waistRadiusToSpacing*signalWidth)/(nGauss-1 + 2*waistRadiusToSpacing);
+    initialGaussCenter = linspace(-signalWidth/2 + initialGaussWaistRadius,signalWidth/2 - initialGaussWaistRadius,nGauss);
+    optimizableObject.WaistRadius(:,1) = initialGaussWaistRadius';
+    optimizableObject.Center(:,1) = initialGaussCenter';
+    [initialVector,minimumVector,maximumVector] = getOptimizableVector( optimizableObject );
+    
+    % Call the nonlinear optimizer
+    [ optimalValue,optimalMeritFunction,optimizedObject,outputStruct ] = ...
+        NonLinearOptimizer( optimizationAlgorithmName,meritFunctionName,...
+        targetValue,weight,optimizableObject,meritFunctionParameters,...
+        optimizationOption,initialVector,minimumVector,maximumVector );
+    
+    % Plot results    
+    nPoints = 500;
+    lowerXTotal = signalCenter - (signalEdgeFactor + 0.5)*signalWidth;
+    upperXTotal = signalCenter + (signalEdgeFactor + 0.5)*signalWidth;
+    % before optimization
+    [ ampValuesDecomposed,xValuesDecomposed ] = plotGauss1D( optimizableObject,lowerXTotal,upperXTotal,nPoints );
+    % after optimization
+    [ ampValuesDecomposed,xValuesDecomposed ] = plotGauss1D( optimizedObject,lowerXTotal,upperXTotal,nPoints );
+end
