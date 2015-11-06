40514282b817536fd7c3274d6b3b94e7ff25e178
Norman G. Worku <normangirma2012@gmail.com>
2015-11-06 15:28:51 +0100

    Some more comments added

--- Enhanced_Matlab_Features/computeSurfaceColor.m
+++ Enhanced_Matlab_Features/computeSurfaceColor.m
@@ -1,17 +1,34 @@
-function [ C ] = computeSurfaceColor( X,Y,Z,peakColor,valleyColor,nLevels,refAxisIndex )
+function [ C ] = computeSurfaceColor( Z,peakColor,valleyColor,nLevels )
     %COMPUTESURFACECOLOR Computes the 3D color matrix for coloring the
-    %surface described by (X,Y,Z). The function first generates a color map
-    %based on the two maxima colors given and maps the values of either
-    %(Z) or (X) or (Y) to the colormap. The reference axis index specifies
-    %which values to use for this mapping.
-    if refAxisIndex == 1
-        P = X;
-    elseif refAxisIndex == 2
-        P = Y;
-    else
-        P = Z;
-    end
+    %surface described by (Z). The function first generates a color map
+    %based on the two maxima colors given and maps the values of 
+    %(Z) to the colormap. 
+    % 
+    % INPUT:
+    % -Z : a 2D matrix with the values used to color the surface.
+    % -peakColor: the RGB vector (1x3) for maximum Z value
+    % -valleyColor: the RGB vector (1x3) for minimum Z value
+    % -nLevels: the number of colors inbetween the max and min values
+    %
+    % OUTPUT:
+    % -C : A 3D matrix which has the color values for each points of the Z
+    % matrix. It can be directly passed to the surf functions to color any
+    % surface.
+    %
+    % Example1: Basic usage
+    % Z = peaks; peakColor = [1,0,0];,valleyColor = [0,1,0]; nLevels = 100;
+    % C = computeSurfaceColor( Z,peakColor,valleyColor,nLevels );
+    % figure; surf(Z,C)
+    %
+    % Example2: Use to plot two surfaces with differnt color grad on the
+    % same axis
+    % Z1 = peaks; peakColor1 = [0,1,0];,valleyColor1 = [0,0,1]; nLevels = 100;
+    % C1 = computeSurfaceColor( Z1,peakColor1,valleyColor1,nLevels );
+    % Z2 = peaks-20; peakColor2 = [1,0,0];,valleyColor2 = [0,0,1]; nLevels = 100;
+    % C2 = computeSurfaceColor( Z2,peakColor2,valleyColor2,nLevels );
+    % figure; surf(Z1,C1); hold on;  surf(Z2,C2);  
     
+    P = Z;
     colorGrad = computeLinearColorGradient(peakColor,valleyColor,nLevels);
     stepSize = (max(max(P) - min(min(P))))/nLevels;
     if stepSize == 0
