44e2980e34518e4b3a6bac312ae66510aca12c7b
Norman G. Worku <normangirma2012@gmail.com>
2016-02-03 17:18:43 +0100

    Fix some remaining issues on displaySystemConfiguration.m function.

--- 4. Optical_System_Package/OpticalSystem/displaySystemConfiguration.m
+++ 4. Optical_System_Package/OpticalSystem/displaySystemConfiguration.m
@@ -19,31 +19,39 @@
   elseif nargin == 5
 
   end
+ disp(' ');
  disp('************************************************************');
  disp('Optical System Configuration Data'); 
  disp('************************************************************');     
+ disp(' ');
  if dispAperture 
+     [systemApertureType] = getSupportedSystemApertureTypes(optSystem.SystemApertureType);
      disp('<<<<<<<<< Aperture Data >>>>>>>>>>>');
-     disp(['Aperture Type : ' optSystem.SystemApertureType]);
+     disp(['Aperture Type : ' systemApertureType]);
      disp(['Aperture Value : ' num2str(optSystem.SystemApertureValue)]);
  end
+ disp(' ');
  if dispGeneral
+     [wavelengthUnit] = getSupportedWavelengthUnits(optSystem.WavelengthUnit);
+     [lensUnit] = getSupportedLensUnits(optSystem.WavelengthUnit);
+     
      disp('<<<<<<<<<< Genral Data >>>>>>>>>>>');
      disp(['Lens Name : ' optSystem.LensName]);
      disp(['Lens Note : ' (optSystem.LensNote)]);  
-     disp(['Wavelength Unit : ' optSystem.WavelengthUnit]);
-     disp(['Lens Unit : ' (optSystem.LensUnit)]);         
+     disp(['Wavelength Unit : ' wavelengthUnit]);
+     disp(['Lens Unit : ' lensUnit]);         
  end
+ disp(' ');
  if dispWavelength
      disp('<<<<<<<<<<<<<< Wavelength Data >>>>>>>>>>>>>>');
-     disp(['Total Number of Wavelength : ' num2str(optSystem.NumberOfWavelengths)]);
+     disp(['Total Number of Wavelength : ' num2str(getNumberOfWavelengths(optSystem))]);
      disp(['Primary Wavelength Index : ' num2str(optSystem.PrimaryWavelengthIndex)]);
      Wavelength_Matrix_Wav_Weight = ({optSystem.WavelengthMatrix});
      celldisp(Wavelength_Matrix_Wav_Weight);
  end
  if dispField 
      disp('<<<<<<<<<<<<<< Feild Point Data >>>>>>>>>>>>>>>');
-     disp(['Total Number of Feild Points : ' num2str(optSystem.NumberOfFieldPoints)]);
+     disp(['Total Number of Feild Points : ' num2str(getNumberOfFieldPoints(optSystem))]);
      Feild_Point_Matrix_X_Y_Weight = ({optSystem.FieldPointMatrix});
      celldisp(Feild_Point_Matrix_X_Y_Weight);  
  end 
