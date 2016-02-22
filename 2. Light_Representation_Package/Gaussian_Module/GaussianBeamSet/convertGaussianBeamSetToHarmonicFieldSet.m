ac2256df3423f39988c5f9502f630fa23269001d
Norman G. Worku <normangirma2012@gmail.com>
2016-02-22 13:39:26 +0100

    Add option to perform coherent superposition of gaussian beams while converting to harmonic field set.

--- 2. Light_Representation_Package/Gaussian_Module/GaussianBeamSet/convertGaussianBeamSetToHarmonicFieldSet.m
+++ 2. Light_Representation_Package/Gaussian_Module/GaussianBeamSet/convertGaussianBeamSetToHarmonicFieldSet.m
@@ -1,14 +1,25 @@
 function [ newHarmonicFieldSet ] = convertGaussianBeamSetToHarmonicFieldSet( ...
-        gaussianBeamSet,Nx,Ny,windowSizeX,windowSizeY )
+        gaussianBeamSet,Nx,Ny,windowSizeX,windowSizeY,coherentSuperposition )
     %convertGaussianBeamSetToHarmonicFieldSet Assume the gaussian
     %beam parameters are all given on the plane perpendicular to the
     %central ray.
+    
+    if nargin < 5
+        disp(['Error: The function convertGaussianBeamSetToHarmonicFieldSet',...
+            'requires atleast 5 input arguments, gaussianBeamSet,Nx,Ny,',...
+            'windowSizeX and windowSizeY.']);
+        newHarmonicFieldSet = NaN;
+        return;
+    end
+    if nargin < 6
+        coherentSuperposition = 0;
+    end
     
     [ generallyAstigmaticGaussianBeamSet ] = ...
         convertSimpleToGenerallyAstigmaticGaussianBeamSet...
         ( gaussianBeamSet );
     [ newHarmonicFieldSet ] = convertGenerallyAstigmaticGaussianBeamToHarmonicField( ...
-        generallyAstigmaticGaussianBeamSet,Nx,Ny,windowSizeX,windowSizeY );
+        generallyAstigmaticGaussianBeamSet,Nx,Ny,windowSizeX,windowSizeY,coherentSuperposition );
 
     
 %     wavelen = gaussianBeamSet.CentralRayWavelength;
