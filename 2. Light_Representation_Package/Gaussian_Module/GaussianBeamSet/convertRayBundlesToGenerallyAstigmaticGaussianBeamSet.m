d301ac930b1b72778a64817531e6d155cc73a523
Norman G. Worku <normangirma2012@gmail.com>
2016-02-23 14:45:34 +0100

    Now ray bundles for multiple gaussian nbeams could be passed as a single ray bundle struct with properties of each beam concatenated.

--- 2. Light_Representation_Package/Gaussian_Module/GaussianBeamSet/convertRayBundlesToGenerallyAstigmaticGaussianBeamSet.m
+++ 2. Light_Representation_Package/Gaussian_Module/GaussianBeamSet/convertRayBundlesToGenerallyAstigmaticGaussianBeamSet.m
@@ -1,6 +1,6 @@
 function [ generallyAstigmaticGaussianBeamSet ] = ...
         convertRayBundlesToGenerallyAstigmaticGaussianBeamSet( ...
-        gaussianBeamRayBundleArray,totalPathLengths )
+        gaussianBeamRayBundle,totalPathLengths )
     %convertRayBundlesToGenerallyAstigmaticGaussianBeamSet Gives the gaussian beam from the five rays
     % used to represent the beams. In the input ray bundle, the ray properties
     % shall be put in agiven order corresonging for each gaussian beam.
@@ -22,36 +22,53 @@ function [ generallyAstigmaticGaussianBeamSet ] = ...
     % Date----------Modified By ---------Modification Detail--------Remark
     % Nov 07,2014   Worku, Norman G.     Original Version
     
-    nArray = length(gaussianBeamRayBundleArray);
-    for kk = 1:nArray
-        gaussianBeamRayBundle = gaussianBeamRayBundleArray(kk);
-        
-        centralRayDirection = gaussianBeamRayBundle.Direction(:,1:5:end);
-        centralRayPosition = gaussianBeamRayBundle.Position(:,1:5:end);
-        centralRayWavelength = gaussianBeamRayBundle.Wavelength(:,1:5:end);
-        nGaussian = length(centralRayWavelength);
-        
-        waistRayXDirection = gaussianBeamRayBundle.Direction(:,2:5:end);
-        waistRayXIntersection = gaussianBeamRayBundle.Position(:,2:5:end);
-        
-        waistRayYDirection = gaussianBeamRayBundle.Direction(:,3:5:end);
-        waistRayYIntersection = gaussianBeamRayBundle.Position(:,3:5:end);
-        
-        divergenceRayXDirection = gaussianBeamRayBundle.Direction(:,4:5:end);
-        divergenceRayXIntersection = gaussianBeamRayBundle.Position(:,4:5:end);
-        
-        divergenceRayYDirection = gaussianBeamRayBundle.Direction(:,5:5:end);
-        divergenceRayYIntersection = gaussianBeamRayBundle.Position(:,5:5:end);
-        
-        complexRay1Position = divergenceRayXIntersection + 1i*waistRayXIntersection;
-        complexRay1Direction = divergenceRayXDirection + 1i*waistRayXDirection;
-        complexRay2Position = divergenceRayYIntersection + 1i*waistRayYIntersection;
-        complexRay2Direction = divergenceRayYDirection + 1i*waistRayYDirection;
-        
-        generallyAstigmaticGaussianBeamSet(kk) = GenerallyAstigmaticGaussianBeamSet(...
-            complexRay1Position,complexRay1Direction,complexRay2Position,...
-            complexRay2Direction,centralRayPosition,centralRayDirection,...
-            centralRayWavelength,totalPathLengths(kk));
+    if nargin < 1
+        disp(['Error: The function convertRayBundlesToGenerallyAstigmaticGaussianBeamSet',...
+            ' needs atleast one argument, gaussianBeamRayBundle.']);
+        generallyAstigmaticGaussianBeamSet = NaN;
+        return;
+    else
+        % Check that the ray bundles passed has parameters which are multiple of 5
+        % since each gaussian beam is represented by 5 rays.
+        nRayParameters = size(gaussianBeamRayBundle.Position,2);
+        if mod(nRayParameters,5) ~= 0
+            disp(['Error: The number of rays represented by the input argument',...
+                ' gaussianBeamRayBundle should be multiple of 5.']);
+            generallyAstigmaticGaussianBeamSet = NaN;
+            return;
+        else
+            nGaussian = size(gaussianBeamRayBundle.Position,2);
+        end
     end
+    if nargin < 2
+        totalPathLengths = zeros(1,nGaussian);
+    end
+    
+    centralRayDirection = gaussianBeamRayBundle.Direction(:,1:5:end);
+    centralRayPosition = gaussianBeamRayBundle.Position(:,1:5:end);
+    centralRayWavelength = gaussianBeamRayBundle.Wavelength(:,1:5:end);
+    
+    
+    waistRayXDirection = gaussianBeamRayBundle.Direction(:,2:5:end);
+    waistRayXIntersection = gaussianBeamRayBundle.Position(:,2:5:end);
+    
+    waistRayYDirection = gaussianBeamRayBundle.Direction(:,3:5:end);
+    waistRayYIntersection = gaussianBeamRayBundle.Position(:,3:5:end);
+    
+    divergenceRayXDirection = gaussianBeamRayBundle.Direction(:,4:5:end);
+    divergenceRayXIntersection = gaussianBeamRayBundle.Position(:,4:5:end);
+    
+    divergenceRayYDirection = gaussianBeamRayBundle.Direction(:,5:5:end);
+    divergenceRayYIntersection = gaussianBeamRayBundle.Position(:,5:5:end);
+    
+    complexRay1Position = divergenceRayXIntersection + 1i*waistRayXIntersection;
+    complexRay1Direction = divergenceRayXDirection + 1i*waistRayXDirection;
+    complexRay2Position = divergenceRayYIntersection + 1i*waistRayYIntersection;
+    complexRay2Direction = divergenceRayYDirection + 1i*waistRayYDirection;
+    
+    generallyAstigmaticGaussianBeamSet = GenerallyAstigmaticGaussianBeamSet(...
+        complexRay1Position,complexRay1Direction,complexRay2Position,...
+        complexRay2Direction,centralRayPosition,centralRayDirection,...
+        centralRayWavelength,totalPathLengths);
 end
 
