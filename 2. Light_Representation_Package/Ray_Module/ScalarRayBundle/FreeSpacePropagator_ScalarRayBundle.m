cf83bf1856793a5889e042152c5c0c851b3fe123
Norman G. Worku <normangirma2012@gmail.com>
2016-02-23 14:54:21 +0100

    now free space propagator returns  a single ray bundle  struct even for multiple propagation distances. The properties will be concatenated in the latter case

--- 2. Light_Representation_Package/Ray_Module/ScalarRayBundle/FreeSpacePropagator_ScalarRayBundle.m
+++ 2. Light_Representation_Package/Ray_Module/ScalarRayBundle/FreeSpacePropagator_ScalarRayBundle.m
@@ -2,10 +2,13 @@ function [ propagatedRayBundle ] = FreeSpacePropagator_ScalarRayBundle(...
         inputScalarRayBundle,propagationDistance )
     %FreeSpacePropagator_ScalarRayBundle Used to compute free space
     %propagation of scalar ray bundles using real ray tracing method.
-    % The code is also vectorized for multiple propagation distance input 
+    % The code is also vectorized for multiple propagation distance input
     
     if nargin < 1
-        inputScalarRayBundle = NaN;
+        disp(['Error: The function FreeSpacePropagator_ScalarRayBundle',...
+            ' requires atleast one input argument inputScalarRayBundle.']);
+        propagatedRayBundle = NaN;
+        return;
     end
     if nargin < 2
         propagationDistance = 0;
@@ -18,12 +21,21 @@ function [ propagatedRayBundle ] = FreeSpacePropagator_ScalarRayBundle(...
             'The first argument should be scalar ray bundle.']);
     end
     
-    tempRayBundle = inputScalarRayBundle;
     nPropagationDistance = length(propagationDistance);
+    % The output raybundle will have parameters with length of
+    % nPropagationDistance*the input ray bundle so that the final ray bundle
+    % will have [Param after prop dist 1, Param for prop dist 2,...]
+    tempPosition = [];
     for kk = 1:nPropagationDistance
-        tempRayBundle.Position = tempRayBundle.Position + ...
-            propagationDistance*tempRayBundle.Direction;
-        propagatedRayBundle (kk) =  tempRayBundle;
+        positionAfterPropgation = inputScalarRayBundle.Position + ...
+            propagationDistance(kk)*inputScalarRayBundle.Direction;
+        tempPosition = cat(2,tempPosition,positionAfterPropgation);
     end
+    
+    tempRayBundle.Position = tempPosition;
+    tempRayBundle.Direction = repmat(inputScalarRayBundle.Direction,[1,nPropagationDistance]);
+    tempRayBundle.Wavelength = repmat(inputScalarRayBundle.Wavelength,[1,nPropagationDistance]);
+    
+    propagatedRayBundle = tempRayBundle;
 end
 
