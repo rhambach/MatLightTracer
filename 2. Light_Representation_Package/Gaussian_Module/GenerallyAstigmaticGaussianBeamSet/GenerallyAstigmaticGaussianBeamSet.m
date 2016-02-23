eae0389fcc3700ec6e4e018a880c2f083faf094b
Norman G. Worku <normangirma2012@gmail.com>
2016-02-23 14:47:18 +0100

    added field called TotalOpticalPathLength for GenerallyAstigmaticGaussianBeamSet struct to hold the phase information associated with the central ray of the beam

--- 2. Light_Representation_Package/Gaussian_Module/GenerallyAstigmaticGaussianBeamSet/GenerallyAstigmaticGaussianBeamSet.m
+++ 2. Light_Representation_Package/Gaussian_Module/GenerallyAstigmaticGaussianBeamSet/GenerallyAstigmaticGaussianBeamSet.m
@@ -1,10 +1,21 @@
 function [ newGaussianBeamSet ] = ...
         GenerallyAstigmaticGaussianBeamSet( complexRay1Position,...
         complexRay1Direction,complexRay2Position,complexRay2Direction,...
-        centralRayPosition,centralRayDirection,wavelength )
+        centralRayPosition,centralRayDirection,wavelength,totalOpticalPathLength )
     %GENERALLYASTIGMATICGAUSSIANBEAMSET Defines a generally astigmatic
     %gaussain beam using the complex ray parameters
     
+    if nargin < 7
+        disp(['Error: The function GenerallyAstigmaticGaussianBeamSet ',...
+            'requires atleast 7 input arguments : complexRay1Position,',...
+            'complexRay1Direction,complexRay2Position,complexRay2Direction,',...
+            'centralRayPosition,centralRayDirection and wavelength.']);
+        newGaussianBeamSet = NaN;
+        return;
+    end
+    if nargin < 8
+        totalOpticalPathLength = 0;
+    end
     newGaussianBeamSet.ComplexRay1Position = complexRay1Position;
     newGaussianBeamSet.ComplexRay1Direction = complexRay1Direction;
     newGaussianBeamSet.ComplexRay2Position = complexRay2Position;
@@ -12,6 +23,7 @@ function [ newGaussianBeamSet ] = ...
     newGaussianBeamSet.CentralRayPosition = centralRayPosition;
     newGaussianBeamSet.CentralRayDirection = centralRayDirection;
     newGaussianBeamSet.Wavelength = wavelength;
+    newGaussianBeamSet.TotalOpticalPathLength = totalOpticalPathLength;
     newGaussianBeamSet.ClassName = 'GenerallyAstigmaticGaussianBeamSet';
 end
 
